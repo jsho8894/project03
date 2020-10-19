9768c8ba9494c601e7ac25909fad0ff4f62b442c
Jdahee <jungdahee@gmail.com>
2020-10-19 10:28:55 +0900

    homme

---
+++ homme/src/main/java/com/homme/controller/HommeController.java
@@ -0,0 +1,61 @@
+package com.homme.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/")
+public class HommeController {
+	
+	@RequestMapping("/tshirt")
+	public String TshirtList() {
+		return "tshirt";
+	}
+	@RequestMapping("/shirt")
+	public String shirtList() {
+		return "shirt";
+	}
+	@RequestMapping("/hoody")
+	public String hoodyList() {
+		return "hoody";
+	}
+	@RequestMapping("/sweater")
+	public String sweaterList() {
+		return "sweater";
+	}
+	@RequestMapping("/jean")
+	public String JeanList() {
+		return "jean";
+	}
+	@RequestMapping("/slacks")
+	public String SlacksList() {
+		return "slacks";
+	}
+	@RequestMapping("/coat")
+	public String CoattList() {
+		return "coat";
+	}
+	@RequestMapping("/jacket")
+	public String JacketList() {
+		return "jacket";
+	}
+	@RequestMapping("/outerc")
+	public String OtercList() {
+		return "outerc";
+	}
+	@RequestMapping("/best")
+	public String BestList() {
+		return "best";
+	}
+	@RequestMapping("/sale")
+	public String SaleList() {
+		return "sale";
+	}
+	@RequestMapping("/buycheck")
+	public String Buycheck() {
+		return "buycheck";
+	}
+
+}
+
